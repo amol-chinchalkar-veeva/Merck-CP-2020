d510f643a7788ae48627442ea058c32e727607c2
achinchalkar <amol.chinchalkar@veeva.com>
2020-03-11 09:09:07 -0400

    Separated zip download and print operation to eliminate ZLIB error.

--- controlled-print-queue/src/main/java/com/veeva/controlledprintqueue/DocumentMover.java
+++ controlled-print-queue/src/main/java/com/veeva/controlledprintqueue/DocumentMover.java
@@ -7,9 +7,15 @@
  */
 package com.veeva.controlledprintqueue;
 
+import java.io.EOFException;
 import java.io.File;
 import java.io.FileOutputStream;
+import java.io.IOException;
 import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipInputStream;
 
@@ -93,23 +99,26 @@ public class DocumentMover implements Runnable {
 			Boolean jobReady = vaultDocumentClient.isJobFinished(Long.toString(jobId), 30, maxRetries);
 			// zip file stream for the control copy download
 			ZipEntry zipEntryFile;
+			Map<String, String> filePathList = new HashMap<String, String>();
 
 			// job is created begin download
 			if (jobReady) {
 
 				logger.info("Job is ready");
-				logger.info("Beginning Download of document: " + this.documentId.getId());
+				logger.info("Initiate downloading of the print copies for document ID: " + this.documentId.getId());
 				File outDir = new File(printFolderLocation);
 				byte[] buffer = new byte[1024];
+				boolean hasPrintFiles = false;
 
 				try (InputStream stream = vaultDocumentClient.downloadByLifecycleJobAction(this.documentId,
 						this.version, this.actionLabel, this.jobId);
 						ZipInputStream zStream = new ZipInputStream(stream);) {
 
 					while ((zipEntryFile = zStream.getNextEntry()) != null) {
-						
-						logger.info("Unzipping started...Mainfetst File Name:"+manifestFileName );
-						// Remove the delivery manifest file as a Print copy is not needed "Delivery Manifest.pdf"
+
+						// logger.info("Unzipping started...Mainfest File Name:" + manifestFileName);
+						// Remove the delivery manifest file as a Print copy is not needed "Delivery
+						// Manifest.pdf"
 						if (!zipEntryFile.getName().equalsIgnoreCase("Delivery Manifest.pdf")) {
 							File newFile = new File(outDir, zipEntryFile.getName());
 							// write to fos
@@ -119,28 +128,43 @@ public class DocumentMover implements Runnable {
 								while ((len = zStream.read(buffer)) > 0) {
 									fos.write(buffer, 0, len);
 								}
+								hasPrintFiles = true;
+								// Added 03-09
+								fos.close();
+								// End 03-09
 							}
-							
-							
-							// initiate the printing using federated Id & PDF Box
-							LPRPrintMode.printFile(outDir + "\\" + zipEntryFile.getName(), printerHostName, printerName,
-									zipEntryFile.getName(), userfederatedId);
 
-							logger.info("File Successfully delivered to printer:" + zipEntryFile.getName());
+							filePathList.put(zipEntryFile.getName(), outDir + "\\" + zipEntryFile.getName());
+//							// initiate the printing using federated Id & PDF Box
+//							LPRPrintMode.printFile(outDir + "\\" + zipEntryFile.getName(), printerHostName, printerName,
+//									zipEntryFile.getName(), userfederatedId);
+//
+//							logger.info("File Successfully delivered to printer:" + zipEntryFile.getName());
+//
+//							// house keeping: delete the print download; Added "\\" on 02/28/2020
+//							File file = new File(outDir + "\\" + zipEntryFile.getName());
+//							if (file.exists()) {
+//								logger.info("Deleting the file:" + outDir + "\\" + zipEntryFile.getName());
+//								file.delete();
+//							}
 
-							// house keeping: delete the print download; Added "\\" on 02/28/2020
-							File file = new File(outDir +"\\"+ zipEntryFile.getName());
-							if (file.exists()) {
-								file.delete();
-							}
 						} // if
 					} // while
+					stream.close();
+				} // try
+				logger.info("Finished Download of document:");
+				if (hasPrintFiles) {
+					// after downlaod all the files in the zip, send them to print queue
+					initiateLPRPrint(filePathList, printerHostName, printerName, userfederatedId);
 				}
 			} else {
 
 				throw new Exception(
 						"Controlled Print Application has timed out and the document has not been sent to the printer");
 			}
+		} catch (EOFException eof) {
+			//
+			logger.error("EOFException while downloading the file", eof);
 		} catch (Exception e) {
 			try {
 				logger.error("Error trying to download the controlled copy to the print folder", e);
@@ -152,5 +176,40 @@ public class DocumentMover implements Runnable {
 
 		logger.info("Download/Print Process Complete");
 
+	}
+
+	/**
+	 * 
+	 * @param filePathList
+	 * @param printerHostName
+	 * @param printerName
+	 * @param fileName
+	 * @param userfederatedId
+	 */
+	private void initiateLPRPrint(Map<String, String> filePathList, String printerHostName, String printerName,
+			String userfederatedId) {
+
+		logger.info("Initiate Print Process.. ");
+		for (String fileName : filePathList.keySet()) {
+
+			// initiate the printing using federated Id & PDF Box
+			try {
+				String filepath = filePathList.get(fileName);
+
+				LPRPrintMode.printFile(filepath, printerHostName, printerName, fileName, userfederatedId);
+
+				logger.info("File Successfully delivered to printer:" + fileName);
+
+				// house keeping: delete the print download; Added "\\" on 02/28/2020
+				File file = new File(filepath);
+				if (file.exists()) {
+					logger.info("Deleting the file:" + filepath);
+					file.delete();
+				}
+			} catch (IOException | InterruptedException e) {
+				logger.error("Error while processing the printing: ", e);
+			}
+		}
+		logger.info("Print Process Complete..");
 	}
 }
